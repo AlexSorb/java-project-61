2dacb827ecd9504ec5f0e64db63e280e259c1d9f
Alexandr Ryabinin <zochsa@gmail.com>
2024-01-21 22:36:23 +0300

    Change game

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -13,20 +13,42 @@ public class Even {
         this.rightAnswer = isEven(currentNumber) ? "yes" : "no";
     }
 
+    /**
+     * Method returns the game rule for the GCD game.
+     * @return game rule.
+     */
     public String getGameRules() {
         return "Answer 'yes' if the number is even, otherwise answer 'no'.";
     }
+
+    /**
+     * The method returns a question for the user.
+     * @return question for user.
+     */
     public String getQuestion() {
         return Integer.toString(currentNumber);
     }
+
+    /**
+     * The method returns the correct answer.
+     * @return right answer
+     */
     public String getRightAnswer() {
         return rightAnswer;
     }
 
+    /**
+     * The method returns true if the correct answer is given.
+     * @param userAnswer
+     * @return true if the answer is correct
+     */
     public boolean isRightAnswer(String userAnswer) {
         return userAnswer.equalsIgnoreCase(rightAnswer);
     }
 
+    /**
+     * The method sets new parameters for the game.
+     */
     public void changeQuestion() {
         currentNumber = Engine.randomIntValue();
         rightAnswer = isEven(currentNumber) ? "yes" : "no";
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -11,9 +11,18 @@ public class Progression {
         changeQuestion();
     }
 
+    /**
+     * Method returns the game rule for the Progression game.
+     * @return game rule.
+     */
     public String getGameRules() {
         return "What number is missing in the progression?";
     }
+
+    /**
+     * The method returns a question for the user.
+     * @return question for user.
+     */
     public String getQuestion() {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < progression.length; i++) {
@@ -27,14 +36,26 @@ public class Progression {
         return result.toString().trim();
     }
 
+    /**
+     * The method returns the correct answer.
+     * @return right answer
+     */
     public String getRightAnswer() {
         return Integer.toString(this.rightAnswer);
     }
 
+    /**
+     * The method returns true if the correct answer is given.
+     * @param userAnswer
+     * @return true if the answer is correct
+     */
     public boolean isRightAnswer(String userAnswer) {
         return userAnswer.equalsIgnoreCase(Integer.toString(this.rightAnswer));
     }
 
+    /**
+     * The method sets new parameters for the game.
+     */
     public void changeQuestion() {
         final int leftBorder = 5;
         final int rightBorder = 10;
