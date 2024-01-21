9e7c5c3ea9599f5fb792a5c387cebfb9ce33867f
Alexandr Ryabinin <zochsa@gmail.com>
2024-01-21 23:38:11 +0300

    Change Prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -53,7 +53,7 @@ public class Prime {
 
     private boolean isPrimeNumber(int num) {
         if (num == 1) {
-            return true;
+            return false;
         }
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
