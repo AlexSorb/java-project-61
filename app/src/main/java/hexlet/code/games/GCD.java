441fddb735029328fe3f058597358dc6390f866c
Alexandr Ryabinin <zochsa@gmail.com>
2024-01-21 22:10:37 +0300

    change GCD

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -14,26 +14,52 @@ public class GCD {
         this.rightAnswer = 0;
         changeQuestion();
     }
+
+    /**
+     * Method returns the game rule for the GCD game.
+     * @return game rule.
+     */
     public String getGameRules() {
         return "Find the greatest common divisor of given numbers.";
     }
+
+    /**
+     * The method returns a question for the user.
+     * @return question for user.
+     */
     public String getQuestion() {
         return this.oneNumber + " " + this.twoNumber;
     }
+
+    /**
+     * The method returns the correct answer.
+     * @return right answer
+     */
     public String getRightAnswer() {
         return Integer.toString(this.rightAnswer);
     }
+
+    /**
+     * The method returns true if the correct answer is given.
+     * @param userAnswer
+     * @return true if the answer is correct
+     */
     public boolean isRightAnswer(String userAnswer) {
         return Integer.parseInt(userAnswer)  == this.rightAnswer;
     }
 
+    /**
+     * The method sets new parameters for the game.
+     */
     public void changeQuestion() {
-        this.oneNumber = Engine.randomIntValue(2, 100);
-        this.twoNumber = Engine.randomIntValue(2, 100);
+        final int leftBorder = 2;
+        final int rightBorder = 100;
+        this.oneNumber = Engine.randomIntValue(leftBorder, rightBorder);
+        this.twoNumber = Engine.randomIntValue(leftBorder, rightBorder);
         this.rightAnswer = getGCD(this.oneNumber, this.twoNumber);
     }
 
-    public static int getGCD(int firstNumber, int secondNumber) {
+    private static int getGCD(int firstNumber, int secondNumber) {
         if (firstNumber == 0 || secondNumber == 0) {
             return -1;
         }
