0bd30f1bd749a28d22d13c9386c8224866a987b2
Alexandr Ryabinin <zochsa@gmail.com>
2024-01-21 22:28:33 +0300

    Change Prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -10,22 +10,42 @@ public class Prime {
         changeQuestion();
     }
 
+    /**
+     * Method returns the game rule for the Prime game.
+     * @return game rule.
+     */
     public String getGameRules() {
         return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     }
 
+    /**
+     * The method returns a question for the user.
+     * @return question for user.
+     */
     public String getQuestion() {
         return Integer.toString(this.number);
     }
 
+    /**
+     * The method returns the correct answer.
+     * @return right answer
+     */
     public String getRightAnswer() {
         return this.rightAnswer;
     }
 
+    /**
+     * The method returns true if the correct answer is given.
+     * @param userAnswer
+     * @return true if the answer is correct
+     */
     public boolean isRightAnswer(String userAnswer) {
         return this.rightAnswer.equalsIgnoreCase(userAnswer);
     }
 
+    /**
+     * The method sets new parameters for the game.
+     */
     public void changeQuestion() {
         this.number = Engine.randomIntValue();
         this.rightAnswer = isPrimeNumber(this.number) ? "yes" : "no";
